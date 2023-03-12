d791afef2a61b426eabfa262a33224c28ea65141
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2023-03-12 17:13:28 +0100

    updated rectangles

--- era5-cube-generation/era5_utils.py
+++ era5-cube-generation/era5_utils.py
@@ -6,32 +6,32 @@ RECTS = [
     # [10, 30, -40, 0], # Southwestern Africa
     # [30, 50, -30, 0], # Southeastern Africa     L
     # [50, 60, 10, 20], # Oman
-    [50, 90, 20, 40], # Middle East
-    [50, 90, 40, 60], # Kazakhstan
+    # [50, 90, 20, 40], # Middle East
+    # [50, 90, 40, 60], # Kazakhstan
     [90, 130, 40, 60], # Mongolia
     [90, 130, 20, 40], # China
+    [100, 110, 10, 20],  # Indochina
+    [110, 160, -40, -20],  # Australia
+    [-80, -60, -30, 0],  # Peru
+    [-60, -40, -30, 0],  # Brazil
+    [-130, -100, 30, 60],  # Western USA
+    [-100, -70, 30, 60],  # Eastern USA
     [70, 100, 0, 20], # India
-    [100, 110, 10, 20], # Indochina
     [100, 120, 0, 10], # Malaysia
     [120, 130, 0, 20], # Philippines
     [130, 170, 50, 60], # Kamchatka
     [130, 150, 30, 50], # Japan
     [100, 120, -10, 0], # Indonesia
     [120, 150, -20, 0], # Northern Australia
-    [110, 160, -40, -20], # Australia
     [140, 150, -50, -40], # Tasmania
     [160, 170, -50, -40], # Southwestern New Zealand
     [170, 180, -50, -30], # Eastern New Zealand
     [-80, -60, -60, -40], # Patagonia
     [-80, -50, -40, -30], # Gran Chaco
-    [-80, -60, -30, 0], # Peru
-    [-60, -40, -30, 0], # Brazil
     [-40, -30, -20, 0], # Eastern Brazil
     [-90, -50, 0, 10], # Colombia
     [-110, -70, 10, 30], # Mexico
     [-120, -110, 20, 30], # Baja California
-    [-130, -100, 30, 60], # Western USA
-    [-100, -70, 30, 60], # Eastern USA
     [-70, -50, 40, 60], # Newfoundland
     [50, 160, 60, 80], # Northern Asia
     [160, 180, 60, 70], # Far East
