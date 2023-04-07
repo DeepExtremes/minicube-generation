b4bf30eb3d479e34a6edd9572ce3142a14f18bfe
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2023-04-07 09:39:15 +0200

    updated rectangles

--- era5-cube-generation/era5_utils.py
+++ era5-cube-generation/era5_utils.py
@@ -4,7 +4,7 @@ RECTS = [
     # [-20, 0, 0, 30], # Western Africa
     # [0, 50, 0, 30], # Northern Africa
     # [10, 30, -40, 0], # Southwestern Africa
-    # [30, 50, -30, 0], # Southeastern Africa     L
+    # [30, 50, -30, 0], # Southeastern Africa
     # [50, 60, 10, 20], # Oman
     # [50, 90, 20, 40], # Middle East
     # [50, 90, 40, 60], # Kazakhstan
@@ -18,34 +18,44 @@ RECTS = [
     # [-100, -70, 30, 60],  # Eastern USA
     # [50, 160, 60, 70],  # Russia
     # [160, 180, 60, 70],  # Far East
-    [-130, -90, 60, 70],  # Nunavut
-    [-140, -130, 50, 70],  # Eastern Alaska
-    [120, 150, -20, 0],  # Northern Australia
-    [-80, -60, -60, -40],  # Patagonia
-    [-150, -140, 60, 70],  # Central Alaska
-    [-110, -70, 10, 30],  # Mexico
-    [-90, -50, 0, 10],  # Colombia
-    [70, 100, 0, 20], # India
-    [100, 120, 0, 10], # Malaysia
-    [120, 130, 0, 20], # Philippines
-    [100, 120, -10, 0], # Indonesia
-    [130, 170, 50, 60], # Kamchatka
-    [130, 150, 30, 50], # Japan
-    [140, 150, -50, -40], # Tasmania
-    [160, 170, -50, -40], # Southwestern New Zealand
-    [170, 180, -50, -30], # Eastern New Zealand
-    [-80, -50, -40, -30], # Gran Chaco
-    [-40, -30, -20, 0], # Eastern Brazil
-    [-120, -110, 20, 30], # Baja California
-    [-70, -50, 40, 60], # Newfoundland
-    [-180, -170, 60, 70],  # Anadyr
-    [-90, -50, 60, 70], # Baffin Island
-    [-50, 10, 60, 70], # Southern Greenland
-    [-170, -150, 50, 70], # Bering Sea
-    # [-90, 10, 70, 80], # Northern Greenland
-    # [50, 160, 70, 80], # Northern Asia
-    # [-130, -90, 70, 80], # Victoria Island
-    # [-160, -150, 70, 80], # Northern Alaska
+    # [-130, -90, 60, 70],  # Nunavut
+    # [-140, -130, 50, 70],  # Eastern Alaska
+    # [-150, -140, 60, 70],  # Central Alaska
+
+    [-80, -50, -40, -30],  # Gran Chaco                 2
+    [130, 170, 50, 60],  # Kamchatka                    3
+    [-90, -50, 60, 70],  # Baffin Island                4
+    [-170, -150, 50, 70],  # Bering Sea                 4
+    [50, 110, 70, 80],  # Taimyr                        6
+    [110, 160, 70, 80],  # Northeastern Siberia         5
+    [-90, -70, 70, 80],  # Southern Ellesmere Island    2
+    [-100, -90, 70, 80],  # Queen Elizabeth Islands     1
+    [-100, -70, 70, 80],  # Northern Ellesmere Island   3
+    [-70, -40, 70, 80],  # Northwestern Greenland       3
+
+    [-70, -50, 40, 60],  # Newfoundland                 3
+    [-110, -70, 10, 30],  # Mexico                      5
+    [-90, -50, 0, 10],  # Colombia                      2
+    [120, 150, -20, 0],  # Northern Australia           1
+    [100, 120, 0, 10],  # Malaysia                      2
+    [-40, -30, -20, 0],  # Eastern Brazil               2
+    [-180, -170, 60, 70],  # Anadyr                     1
+
+    [70, 100, 0, 20],  # India                          4
+    [-50, 10, 60, 70],  # Southern Greenland            4
+    [130, 150, 30, 50],  # Japan                        3
+    [-80, -60, -60, -40],  # Patagonia                  2
+    [120, 130, 0, 20],  # Philippines                   2
+    [100, 120, -10, 0],  # Indonesia                    2
+    [140, 150, -50, -40],  # Tasmania                   1
+    [170, 180, -50, -30],  # Eastern New Zealand        2
+    [160, 170, -50, -40],  # Southwestern New Zealand   1
+    [-120, -110, 20, 30],  # Baja California            1
+
+    # [-70, 10, 70, 80], # Greenland                      6
+    # [-130, -100, 70, 80], # Victoria Island             3
+    # [-40, -10, 70, 80],  # Northeastern Greenland       3
+    # [-160, -150, 70, 80], # Northern Alaska             1
 ]
 
 
