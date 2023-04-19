65d430540fe091365d98b557686fe0c2ab6af2b9
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2023-04-19 17:56:46 +0200

    adjusted cloud mask to performance improved computation

--- minicube-generation/03-cube-generation/maskaycloudmask.py
+++ minicube-generation/03-cube-generation/maskaycloudmask.py
@@ -1,6 +1,7 @@
 import maskay
-from maskay.library.unetmobv2 import UnetMobV2
 import xarray as xr
+
+from unetmobv3 import UnetMobV3
 
 _CLOUD_MASK_NAMES = [
     'cloud_clear_prob', 'cloud_thick_prob',
@@ -8,53 +9,36 @@ _CLOUD_MASK_NAMES = [
 ]
 
 
-def _compute_mask_for_timestep(ds_slice: xr.Dataset,
-                               model,
-                               predictor: maskay.Predictor) -> xr.Dataset:
-    print(f'Processing cloud mask for {ds_slice.time.values}')
-    tensor = maskay.TensorSat(
-        Aerosol=ds_slice.B01 * 10000,
-        Blue=ds_slice.B02 * 10000,
-        Green=ds_slice.B03 * 10000,
-        Red=ds_slice.B04 * 10000,
-        RedEdge1=ds_slice.B05 * 10000,
-        RedEdge2=ds_slice.B06 * 10000,
-        RedEdge3=ds_slice.B07 * 10000,
-        NIR=ds_slice.B08 * 10000,
-        NIR2=ds_slice.B8A * 10000,
-        WaterVapor=ds_slice.B09 * 10000,
-        Cirrus=ds_slice.B10 * 10000,
-        SWIR1=ds_slice.B11 * 10000,
-        SWIR2=ds_slice.B12 * 10000,
-        cache=True,
-        align=False
-    )
-    cm_ds = predictor.predict(model, tensor).to_dataset(dim='band')
-    data_arrays = []
-    for index in range(len(_CLOUD_MASK_NAMES)):
-        data_arrays.append(cm_ds[index].expand_dims(dim={
-            'time': ds_slice.expand_dims(dim='time').time})
-                           .rename(_CLOUD_MASK_NAMES[index]))
-    return xr.combine_by_coords(data_arrays)
-
-
-def compute_cloud_mask(ds_source: xr.Dataset) -> xr.Dataset:
-    model = UnetMobV2()
+def compute_cloud_mask(ds_source: xr.Dataset,
+                       device: str = 'cpu',
+                       batch_size: int = 15) -> xr.Dataset:
+    model = UnetMobV3()
     predictor = maskay.Predictor(
         cropsize=128,
         overlap=0,
-        device="cpu",
-        quiet=False
+        device=device,
+        quiet=False,
+        batchsize=batch_size
+    )
+    tensor = maskay.TensorSat(
+        Aerosol=ds_source.B01 * 10000,
+        Blue=ds_source.B02 * 10000,
+        Green=ds_source.B03 * 10000,
+        Red=ds_source.B04 * 10000,
+        RedEdge1=ds_source.B05 * 10000,
+        RedEdge2=ds_source.B06 * 10000,
+        RedEdge3=ds_source.B07 * 10000,
+        NIR=ds_source.B08 * 10000,
+        NIR2=ds_source.B8A * 10000,
+        WaterVapor=ds_source.B09 * 10000,
+        Cirrus=ds_source.B10 * 10000,
+        SWIR1=ds_source.B11 * 10000,
+        SWIR2=ds_source.B12 * 10000,
+        cache=True,
+        align=False
     )
 
-    ds_target = _compute_mask_for_timestep(
-        ds_source.isel(time=0), model, predictor
-    )
-    if len(ds_source.time) > 1:
-        for ti in range(1, len(ds_source.time)):
-            ds_slice = ds_source.isel(time=ti)
-            ds_time_step = _compute_mask_for_timestep(
-                ds_slice, model, predictor
-            )
-            ds_target = xr.concat([ds_target, ds_time_step], dim='time')
-    return ds_target
+    cm_ds = predictor.predict(model, tensor)
+    cm_ds = cm_ds.assign(crs=ds_source.crs)
+
+    return cm_ds
