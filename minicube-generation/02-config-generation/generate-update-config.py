03b448097498fea9ed6500b04a75cf41b43a84af
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2023-07-20 08:09:48 +0200

    work on adapting update configurations to get information from registry

--- minicube-generation/02-config-generation/generate-update-config.py
+++ minicube-generation/02-config-generation/generate-update-config.py
@@ -2,6 +2,7 @@ import sys
 from typing import List
 
 from configgencreation import get_available_components
+from configgencreation import get_list_of_csv_location_ids
 from configgencreation import get_store
 from configgencreation import create_update_config
 
@@ -12,12 +13,13 @@ def _update_components(mc_path: str, components_to_update: List[str]):
     mc_bucket = '/'.join(split_path[:-1])
     mc_store = get_store(mc_bucket)
     available_components = get_available_components()
+    csv_location_ids = get_list_of_csv_location_ids()
     for component in components_to_update:
         if component not in available_components:
             raise ValueError(f'Component {component} was not found. '
                              f'Must be one of {available_components}')
     mc = mc_store.open_data(minicube_id)
-    create_update_config(mc, mc_path, components_to_update)
+    create_update_config(mc, mc_path, components_to_update, csv_location_ids)
 
 
 if __name__ == "__main__":
--- minicube-generation/02-config-generation/generate-update-configs.py
+++ minicube-generation/02-config-generation/generate-update-configs.py
@@ -1,27 +1,37 @@
+import geopandas as gpd
+import pandas as pd
 import sys
 from typing import List
 
 from configgencreation import get_available_components
+from configgencreation import get_fs
 from configgencreation import get_list_of_csv_location_ids
 from configgencreation import get_store
 from configgencreation import create_update_config
 
 
-def _update_components(mc_bucket: str, components_to_update: List[str]):
-    mc_store = get_store(mc_bucket)
-    minicube_ids = mc_store.list_data_ids()
+from constants import MC_REGISTRY
+
+
+def _update_components(base_version: str, components_to_update: List[str]):
+    mc_base_store = get_store('deepextremes-minicubes/base/')
     available_components = get_available_components()
     for component in components_to_update:
         if component not in available_components:
-            raise ValueError(f'Component {component} was not found. Must be one of '
-                             f'{available_components}')
+            raise ValueError(f'Component {component} was not found. '
+                             f'Must be one of {available_components}')
     csv_location_ids = get_list_of_csv_location_ids()
-    for minicube_id in minicube_ids:
-        mc = mc_store.open_data(minicube_id)
-        mc_path = f'{mc_bucket}/{minicube_id}'
-        create_update_config(
-            mc, mc_path, components_to_update, csv_location_ids
-        )
+    fs = get_fs()
+    with fs.open(MC_REGISTRY, 'r') as gjreg:
+        gpdreg = gpd.GeoDataFrame(pd.read_csv(gjreg))
+        entries = gpdreg.loc[gpdreg.version == base_version]
+        minicube_ids = list(entries.mc_id.values)
+        for minicube_id in minicube_ids:
+            mc = mc_base_store.open_data(minicube_id)
+            mc_path = f'deepextremes-minicubes/base/{minicube_id}'
+            create_update_config(
+                mc, mc_path, components_to_update, csv_location_ids
+            )
 
 
 if __name__ == "__main__":
@@ -29,6 +39,6 @@ if __name__ == "__main__":
         raise ValueError(f'This function requires a path to a bucket containing '
                          f'minicubes and a list of components which shall be updated. '
                          f'Available: components: {get_available_components()}')
-    mc_bucket = sys.argv[1]
+    base_version = sys.argv[1]
     components_to_update = sys.argv[2:]
-    _update_components(mc_bucket, components_to_update)
+    _update_components(base_version, components_to_update)
